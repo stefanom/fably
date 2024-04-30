2363e711108a7f846928b233ad60a2cc4cb38e7a
Stefano Mazzocchi <smazzocchi@singularity6.com>
2024-04-30 14:37:59 -0700

    more tuning

--- startup/start_leds.py
+++ startup/start_leds.py
@@ -6,8 +6,8 @@ NUM_LED = 3
 BRIGHTNESS = 15
 
 def main():
-    my_cycle = colorschemes.TheaterChase(num_led=NUM_LED, pause_value=0.04,
-                                         num_steps_per_cycle=35, num_cycles=2, order='rgb',
+    my_cycle = colorschemes.TheaterChase(num_led=NUM_LED, pause_value=0.03,
+                                         num_steps_per_cycle=35, num_cycles=1, order='rgb',
                                          global_brightness=BRIGHTNESS)
     my_cycle.start()
 
